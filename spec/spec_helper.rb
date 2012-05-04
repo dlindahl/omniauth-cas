8bba7e15465374cf7e8c51ac62a5349fd322add7
Derek Lindahl <dlindahl@customink.com>
2012-05-04 12:24:02 -0400

    Updated RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'bundler/setup'
 require 'bourne'
 
 RSpec.configure do |c|
-  c.filter_run :focus => true
+  c.filter_run focus: true
   c.run_all_when_everything_filtered = true
   c.treat_symbols_as_metadata_keys_with_true_values = true
   c.mock_with :mocha
@@ -12,5 +12,9 @@ require 'simplecov'
 SimpleCov.start
 
 require 'rack/test'
+# TODO: Remove this.
+# https://github.com/bblimke/webmock/issues/64
+# https://github.com/bblimke/webmock/commit/9d255f118a6a39d297856fa83302aca1577b2c03#commitcomment-192888
+require 'rspec/expectations'
 require 'webmock/rspec'
 require 'omniauth-cas'
