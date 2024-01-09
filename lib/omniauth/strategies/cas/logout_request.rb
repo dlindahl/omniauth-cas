3443b5115a540cb762246360ff984678f85e11b1
Geremia Taglialatela <tagliala.dev@gmail.com>
2024-01-09 19:49:23 +0100

    Fix safe RuboCop offenses in production code

--- lib/omniauth/strategies/cas/logout_request.rb
+++ lib/omniauth/strategies/cas/logout_request.rb
@@ -3,7 +3,8 @@ module OmniAuth
     class CAS
       class LogoutRequest
         def initialize(strategy, request)
-          @strategy, @request = strategy, request
+          @strategy = strategy
+          @request = request
         end
 
         def call(options = {})
@@ -11,10 +12,10 @@ module OmniAuth
 
           begin
             result = single_sign_out_callback.call(*logout_request)
-          rescue StandardError => err
-            return @strategy.fail! :logout_request, err
+          rescue StandardError => e
+            @strategy.fail! :logout_request, e
           else
-            result = [200,{},'OK'] if result == true || result.nil?
+            result = [200, {}, 'OK'] if result == true || result.nil?
           ensure
             return unless result
 
@@ -22,18 +23,18 @@ module OmniAuth
             # when Rack::Response#new wants [body,status,headers]? Additionally,
             # why does Rack::Response differ in argument order from the usual
             # Rack-like [status,headers,body] array?
-            return Rack::Response.new(result[2],result[0],result[1]).finish
+            return Rack::Response.new(result[2], result[0], result[1]).finish
           end
         end
 
-      private
+        private
 
         def logout_request
           @logout_request ||= begin
             saml = Nokogiri.parse(@request.params['logoutRequest'])
             name_id = saml.xpath('//saml:NameID').text
             sess_idx = saml.xpath('//samlp:SessionIndex').text
-            inject_params(name_id:name_id, session_index:sess_idx)
+            inject_params(name_id: name_id, session_index: sess_idx)
             @request
           end
         end
@@ -42,7 +43,7 @@ module OmniAuth
           rack_input = @request.env['rack.input'].read
           params = Rack::Utils.parse_query(rack_input, '&').merge new_params
           @request.env['rack.input'] = StringIO.new(Rack::Utils.build_query(params))
-        rescue
+        rescue StandardError
           # A no-op intended to ensure that the ensure block is run
           raise
         ensure
